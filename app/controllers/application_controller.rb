3d3b445238df08a9d6162999d33412aff6682ff1
hxie0 <xhy@chicagobooth.edu>
2021-07-13 21:54:36 +0000

    RPS completed!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,24 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({:template => "game_templates/rules.html.erb"})
+  end
+
   def play_rock
     # write code for playing rock
     #render({:plain => rand(100)})
     #render({:html => "<h1>Hello, world!</h1>".html_safe})
     render({:template => "game_templates/user_rock.html.erb"})
     #redirect_to("https://www.wikipedia.org")
+  end
 
+  def play_paper
+    render({:template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+    render({:template => "game_templates/user_scissors.html.erb"})
   end
 
 end
