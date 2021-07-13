098f5c04ca022ebe7fdfabf50aac5efb8a62ff1f
hxie0 <xhy@chicagobooth.edu>
2021-07-13 21:20:35 +0000

    in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    # write code for playing rock
+    #render({:plain => rand(100)})
+    #render({:html => "<h1>Hello, world!</h1>".html_safe})
+    render({:template => "game_templates/user_rock.html.erb"})
+    #redirect_to("https://www.wikipedia.org")
+
+  end
+
 end
